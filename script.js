b6fd150db4bcfe50eb9376396c940cbcfec993fc
janetmei <179970314+janetmei@users.noreply.github.com>
2024-11-11 13:31:45 -0300

    base app clima

---
+++ script.js
@@ -0,0 +1,40 @@
+let urlBase ='https://api.openweathermap.org/data/2.5/weather'
+let api_key = '61be39b5622677c0942124cf637bda7b'
+let difKelvin = 273.15
+
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+    const ciudad = document.getElementById('ciudadEntrada').value
+    if(ciudad){
+        fetchDatosClima(ciudad)
+    }
+})
+
+function fetchDatosClima(ciudad){
+    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data))
+}
+
+function mostrarDatosClima(data){
+    const divDatosClima = document.getElementById('datosClima')
+    divDatosClima.innerHTML=''
+
+    const ciudadNombre = data.name
+    const temperatura = data.main.temp
+    const descripcion = data.weather[0].description
+    
+    // Crear elemento html
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent = ciudadNombre
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}°C`
+
+    const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `La descripción meteorológica es: ${descripcion}`
+    
+    // Incluir loe elementos html en el div
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(descripcionInfo)
+}
