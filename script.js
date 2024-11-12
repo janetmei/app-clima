a74809fcb998404a3ac95b8a69cae47083e71d4a
janetmei <179970314+janetmei@users.noreply.github.com>
2024-11-12 09:29:24 -0300

    Funciones agregadas a javascrpit

--- script.js
+++ script.js
@@ -20,21 +20,36 @@ function mostrarDatosClima(data){
     divDatosClima.innerHTML=''
 
     const ciudadNombre = data.name
+    const paisNombre = data.sys.country
     const temperatura = data.main.temp
+    const humedad = data.main.humidity
     const descripcion = data.weather[0].description
-    
+    const estadoClima = data.weather[0].main
+    const icono = data.weather[0].icon
+
     // Crear elementos html
     const ciudadTitulo = document.createElement('h2')
-    ciudadTitulo.textContent = ciudadNombre
+    ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
 
     const temperaturaInfo = document.createElement('p')
     temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}°C`
+    
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `La humedad es: ${humedad}%`
 
+    const iconoInfo = document.createElement('img')
+    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
+    
     const descripcionInfo = document.createElement('p')
     descripcionInfo.textContent = `La descripción meteorológica es: ${descripcion}`
     
     // Incluir loe elementos html en el div
     divDatosClima.appendChild(ciudadTitulo)
     divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(iconoInfo)
     divDatosClima.appendChild(descripcionInfo)
 }
+
+
+
